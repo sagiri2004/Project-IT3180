b0c57b535fc606641d0f19d96688b61d258bba59
19010853 <khoinguyenminhk37@gmail.com>
2025-04-23 00:19:25 +0700

    Design login-register along with some utilities folders and files

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+// import React, { lazy, Suspense } from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import { BrowserRouter } from 'react-router-dom';
+
+// const App = lazy(() => import('./App'));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <BrowserRouter>
+//         <Suspense fallback={<div>Loading...</div>}>
+//             <App />
+//         </Suspense>
+//     </BrowserRouter>
+// );
